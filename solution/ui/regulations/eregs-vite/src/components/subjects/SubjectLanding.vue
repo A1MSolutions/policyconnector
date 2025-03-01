7ed0293c8a065c3f52d4583eb85a6b7e1ad8a89a
Britta <britta.gustafson@a1msolutions.com>
2025-02-28 23:22:52 -0800

    Update EO homepage and sidebar copy

--- solution/ui/regulations/eregs-vite/src/components/subjects/SubjectLanding.vue
+++ solution/ui/regulations/eregs-vite/src/components/subjects/SubjectLanding.vue
@@ -14,7 +14,19 @@ const $route = useRoute();
 
 <template>
     <div class="subj-landing__container">
-    <p>TBD</p>
+    <h1>Research Executive Actions</h1>
+    
+    <p><strong>Pick an Executive Order or Memo</strong> to find related guidance, statutes, and prior Executive Orders.</p>
+    
+    <span>Or browse by category:</span>
+    <ul>
+    	<li><a href="?categories=9">Court Cases</a></li>
+    	<li><a href="?categories=5">Executive Orders and Memos</a></li>
+    	<li><a href="?categories=11">OPM Memos</a></li>
+    	<li><a href="?categories=10">OPM Publications</a></li>
+    	<li><a href="?categories=13">Merit Systems Protection Board Publications</a></li>
+    	<li><a href="?categories=2">Congressional Research Reports</a></li>
+    </ul>
     </div>
 </template>
 
