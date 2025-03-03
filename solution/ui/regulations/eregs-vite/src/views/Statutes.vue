ea6453b0fbadd77a27c7f96dad86eaff3cfa6960
Britta <britta.gustafson@a1msolutions.com>
2025-03-03 14:15:51 -0800

    Add OPM tables

--- solution/ui/regulations/eregs-vite/src/views/Statutes.vue
+++ solution/ui/regulations/eregs-vite/src/views/Statutes.vue
@@ -174,12 +174,23 @@ getStatutesArray();
                     <div>
 						<h1>Statute Reference</h1>
 						
-						<p>Provided for convenience, this is a table of contents for <a href="https://uscode.house.gov/browse/prelim@title5&edition=prelim">5 U.S.C.</a> Part III, with links to the <a href="https://uscode.house.gov/">House U.S. Code website</a>. Jump to a section using Ctrl+F (or Command+F on a Mac).</p>
-						
-						<h2>Title 5 — Government Organization and Employees</h2>
+						<h2>Tables for Policy Areas</h2>
+
+						<p>OPM provides the following indexes of laws and regulations:</p>
+						<ul style="font-weight: 600;">
+						  <li><a href="https://www.opm.gov/policy-data-oversight/pay-leave/leave-administration/#url=Reference-Materials" class="external" target="_blank" rel="noopener noreferrer">Leave</a></li>
+						  <li><a href="https://www.opm.gov/policy-data-oversight/pay-leave/pay-administration/fact-sheets/laws-regulations-and-other-references-related-to-pay-administration/" class="external" target="_blank" rel="noopener noreferrer">Pay Administration</a></li>
+						  <li><a href="https://www.opm.gov/policy-data-oversight/performance-management/legal-citations/" class="external" target="_blank" rel="noopener noreferrer">Performance Management</a></li>
+						  <li><a href="https://www.opm.gov/policy-data-oversight/senior-executive-service/ses-desk-guide/statutes-regulations-and-desk-guide-citations/" class="external" target="_blank" rel="noopener noreferrer">Senior Executive Service</a></li>
+						  <li><a href="https://www.opm.gov/wiki/training/Training-Administration-Fast-Facts-Index.ashx" class="external" target="_blank" rel="noopener noreferrer">Training and Development</a></li>
+						</ul>
+
+						<h2>U.S.C. Title 5 — Government Organization and Employees</h2>
 						
 						<h3>Part III — Employees</h3>
-						
+
+						<p>This is a table of contents with links to <a href="https://uscode.house.gov/browse/prelim@title5&edition=prelim" class="external" target="_blank" rel="noopener noreferrer">5 U.S.C. on the House U.S. Code website</a>. Jump to a section using Ctrl+F (or Command+F on a Mac).</p>
+																		
 						<h4>Subpart A — General Provisions</h4>
 						
 						<h5>Chapter 21 — Definitions</h5>
