171eb93bc985544535c5f93728e5c78578964089
Britta Gustafson <britta.gustafson@a1msolutions.com>
2025-03-07 15:44:09 -0800

    Set up new fonts (#78)
    * Set up new fonts
    
    * Swapped the primary and secondary
    
    * Shouldn't need the old fonts anymore

--- solution/ui/regulations/eregs-vite/src/App.vue
+++ solution/ui/regulations/eregs-vite/src/App.vue
@@ -39,7 +39,7 @@ ul {
 }
 
 .v-application {
-    font-family: Open Sans, Helvetica, sans-serif;
+    font-family: Roboto Slab, Helvetica, sans-serif;
 
     .v-application--wrap {
         min-height: unset;
