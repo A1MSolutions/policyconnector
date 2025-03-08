27ea11ecb43b511b1e4d7a4deead419e614d5628
Britta <britta.gustafson@a1msolutions.com>
2025-03-07 16:14:13 -0800

    Remove unused code in statutes page

--- solution/ui/regulations/eregs-vite/src/views/Statutes.vue
+++ solution/ui/regulations/eregs-vite/src/views/Statutes.vue
@@ -1,18 +1,8 @@
 <script setup>
-import { computed, inject, onMounted, onUnmounted, ref, watch } from "vue";
+import { inject } from "vue";
 import { useRoute } from "vue-router";
 
-import { ACT_TYPES } from "eregsComponentLib/src/components/shared-components/Statutes/utils/enums.js";
-import { getStatutes, getStatutesActs } from "utilities/api.js";
-import { shapeTitlesResponse } from "utilities/utils.js";
-
-import SimpleSpinner from "eregsComponentLib/src/components/SimpleSpinner.vue";
-import StatuteSelector from "eregsComponentLib/src/components/shared-components/Statutes/StatuteSelector.vue";
-import StatuteTable from "eregsComponentLib/src/components/shared-components/Statutes/StatuteTable.vue";
-import TableCaption from "eregsComponentLib/src/components/shared-components/Statutes/TableCaption.vue";
-
 import AccessLink from "@/components/AccessLink.vue";
-import Banner from "@/components/Banner.vue";
 import HeaderComponent from "@/components/header/HeaderComponent.vue";
 import HeaderLinks from "@/components/header/HeaderLinks.vue";
 import HeaderSearch from "@/components/header/HeaderSearch.vue";
@@ -21,121 +11,12 @@ import HeaderUserWidget from "@/components/header/HeaderUserWidget.vue";
 
 const adminUrl = inject("adminUrl");
 const apiUrl = inject("apiUrl");
-const customLoginUrl = inject("customLoginUrl");
 const homeUrl = inject("homeUrl");
 const isAuthenticated = inject("isAuthenticated");
 const searchUrl = inject("searchUrl");
 const subjectsUrl = inject("subjectsUrl");
 const username = inject("username");
 
-// get route query params
-const $route = useRoute();
-
-// validate query params to make sure they're in the enum?
-const queryParams = ref({
-    act: $route?.query?.act ?? "ssa",
-    title: $route?.query?.title ?? "19",
-});
-
-// Act titles -- state, fetch method, parse method
-const acts = ref({
-    results: [],
-    loading: true,
-});
-
-const getActTitles = async () => {
-    try {
-        const actsArray = await getStatutesActs({
-            apiUrl,
-        });
-
-        acts.value.results = actsArray;
-    } catch (error) {
-        console.error(error);
-    } finally {
-        acts.value.loading = false;
-    }
-};
-
-const parsedTitles = computed(() =>
-    shapeTitlesResponse({
-        actsResults: acts.value.results,
-        actTypes: ACT_TYPES,
-    })
-);
-
-// Statutes -- state and fetch method
-const statutes = ref({
-    results: [],
-    loading: true,
-});
-
-const getStatutesArray = async () => {
-    statutes.value.loading = true;
-
-    try {
-        const statutesArray = await getStatutes({
-            act: ACT_TYPES[queryParams.value.act],
-            apiUrl,
-            title: queryParams.value.title,
-        });
-
-        statutes.value.results = statutesArray;
-    } catch (error) {
-        console.error(error);
-    } finally {
-        statutes.value.loading = false;
-    }
-};
-
-// watch query params and fetch statutes
-watch(
-    () => $route.query,
-    (newParams) => {
-        queryParams.value = {
-            act: newParams.act,
-            title: newParams.title,
-        };
-    }
-);
-
-watch(
-    () => queryParams.value,
-    async () => {
-        await getStatutesArray();
-    }
-);
-
-// Watch layout
-const windowWidth = ref(window.innerWidth);
-const isNarrow = computed(() => windowWidth.value < 1024);
-
-// Watch Banner left margin
-const bannerRef = ref(null);
-const bannerLeftMargin = ref(0);
-
-const getBannerLeftMargin = () => {
-    const bannerContent = bannerRef.value.$el
-        .getElementsByClassName("content")
-        .item(0);
-
-    bannerLeftMargin.value = window.getComputedStyle(bannerContent).marginLeft;
-};
-
-const onWidthChange = () => {
-    windowWidth.value = window.innerWidth;
-    getBannerLeftMargin();
-};
-
-onMounted(() => {
-    window.addEventListener("resize", onWidthChange);
-    getBannerLeftMargin();
-});
-onUnmounted(() => window.removeEventListener("resize", onWidthChange));
-
-// On load
-getActTitles();
-getStatutesArray();
 </script>
 
 <template>
@@ -164,13 +45,8 @@ getStatutesArray();
             </HeaderComponent>
         </header>
         <div id="statuteApp" class="statute-view">
-           <!-- <Banner ref="bannerRef" title="Statute Reference">
-                <template #description>
-                    <p>Provided for convenience, this is a table of contents for <a href="https://uscode.house.gov/browse/prelim@title5&edition=prelim">5 U.S.C.</a> Part III, with links to the <a href="https://uscode.house.gov/">House U.S. Code website</a>. Jump to a section using Ctrl+F (or Command+F on a Mac).</p>
-                </template>
-            </Banner>-->
             <div id="main-content" class="statute__container">
-                <div class="content" :style="{ marginLeft: bannerLeftMargin }">
+                <div class="content">
                     <div>
 						<h1>Statute Reference</h1>
 						
