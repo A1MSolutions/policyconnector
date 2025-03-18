177aafc1fa31a98c9e94ae53733ecd555a156d71
Britta <britta.gustafson@a1msolutions.com>
2025-03-17 21:47:51 -0700

    Adjust flash banner text

--- solution/ui/regulations/eregs-vite/src/components/FlashBanner.vue
+++ solution/ui/regulations/eregs-vite/src/components/FlashBanner.vue
@@ -1,5 +1,5 @@
 <template>
     <div class="flash-banner" role="banner">
-        <span class="greeting">Policy Connector is a customizable knowledge management tool for any policy area. Your requirements, our proven platform. <a href="{% url 'about' %}">Get started →</a></span>
+        <span class="greeting">This is a demo of Policy Connector, a customizable knowledge management tool for any policy area. Your requirements, our proven platform. <a href="{% url 'about' %}">Learn more →</a></span>
     </div>
 </template>
